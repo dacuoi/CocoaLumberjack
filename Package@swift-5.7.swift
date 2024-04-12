faf4afd3ec4cc9f6d23c337cf695a21295791a69
dacuoi <paintbsd@gmail.com>
2024-04-12 16:44:02 +0700

    Update Package@swift-5.7.swift
    Temp: Change WatchOS to V5 for conflict swift-log (SPM minimum là v5)

--- Package@swift-5.7.swift
+++ Package@swift-5.7.swift
@@ -9,7 +9,7 @@ let package = Package(
         .macOS(.v10_13),
         .iOS(.v11),
         .tvOS(.v11),
-        .watchOS(.v4),
+        .watchOS(.v5),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
